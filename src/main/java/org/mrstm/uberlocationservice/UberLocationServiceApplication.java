f7b2884662f25a0c785f2631bb69a8b1ea88df28
Satyam Prajapati <mrstm.cpp@gmail.com>
2025-10-07 20:31:19 +0530

    Kafka msg receiving error resolved

--- src/main/java/org/mrstm/uberlocationservice/UberLocationServiceApplication.java
+++ src/main/java/org/mrstm/uberlocationservice/UberLocationServiceApplication.java
@@ -2,6 +2,7 @@ package org.mrstm.uberlocationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
--- src/main/java/org/mrstm/uberlocationservice/consumers/BookingCreatedConsumer.java
+++ src/main/java/org/mrstm/uberlocationservice/consumers/BookingCreatedConsumer.java
@@ -1,10 +1,11 @@
 package org.mrstm.uberlocationservice.consumers;
 
-import org.mrstm.uberlocationservice.dto.Booking.BookingCreatedEvent;
+
+import org.mrstm.uberentityservice.dto.booking.BookingCreatedEvent;
 import org.mrstm.uberlocationservice.kafka.KafkaTopics;
 import org.mrstm.uberlocationservice.services.LocationService;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
+
 import org.springframework.stereotype.Service;
 
 
@@ -20,7 +21,7 @@ public class BookingCreatedConsumer {
 
     @KafkaListener(topics = KafkaTopics.BOOKING_CREATED, groupId = "location-group")
     public void consumeBookingCreated(BookingCreatedEvent payload) {
-        System.out.println(payload.getLatitude());
+        System.out.println(payload.toString());
 //        List<DriverLocationDto> nearbyDrivers = locationService.getNearbyDrivers(payload.getPassengerId().toString());
     }
 
